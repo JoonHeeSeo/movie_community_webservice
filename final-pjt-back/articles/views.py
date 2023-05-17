26b206f5e1891efad86ccf4a50a0aaa7071e4e5d
서준희 <seojoonhee10@gmail.com>
2023-05-17 14:47:08 +0900

    article/<article_pk> part

--- final-pjt-back/articles/views.py
+++ final-pjt-back/articles/views.py
@@ -25,3 +25,30 @@ def article_list(request):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def article_detail(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+
+
+
+
+
+
+
