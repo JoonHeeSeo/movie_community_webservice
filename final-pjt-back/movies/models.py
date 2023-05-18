0d343a86fe1751a17915814df1351e7b50feb4ca
서준희 <seojoonhee10@gmail.com>
2023-05-18 09:17:05 +0900

    get_movies_API view 수정중

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -5,20 +5,21 @@ from django.db import models
 
 class Movie(models.Model):
     
-    adult = models.TextField()
-    backdrop_path = models.TextField()
-    genre_ids = models.TextField()
-    movie_id = models.IntegerField()
-    original_language = models.TextField()
-    original_title = models.TextField()
-    overview = models.TextField()
-    popularity = models.IntegerField()
-    poster_path = models.TextField()
-    release_date = models.TextField()
-    title = models.TextField()
-    video = models.TextField()
-    vote_average = models.FloatField()
-    vote_count = models.IntegerField()
+
+    # movie_id를 제외한 모든 필드 null과 blank 허용
+    adult = models.TextField(null=True, blank=True)
+    backdrop_path = models.TextField(null=True, blank=True)
+    genre_ids = models.TextField(null=True, blank=True)
+    movie_id = models.IntegerField(null=True, blank=True)
+    original_language = models.TextField(null=True, blank=True)
+    original_title = models.TextField(null=True, blank=True)
+    overview = models.TextField(null=True, blank=True)
+    popularity = models.IntegerField(null=True, blank=True)
+    poster_path = models.TextField(null=True, blank=True)
+    release_date = models.TextField(null=True, blank=True)
+    title = models.TextField(null=True, blank=True)
+    video = models.TextField(null=True, blank=True)
+    vote_average = models.FloatField(null=True, blank=True)
+    vote_count = models.IntegerField(null=True, blank=True)
 
 
-    # total = models.JSONField()
--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -8,53 +8,72 @@ import requests
 
 
 
-API_KEY = 'af5292844a6af1d68203e1c0b3104130'
-API_PAGE = 1
-API_URL = f'https://api.themoviedb.org/3/movie/top_rated?api_key={API_KEY}&language=ko-kr&page={API_PAGE}'
+# API_KEY = 'af5292844a6af1d68203e1c0b3104130'
+# API_PAGE = 1
+# API_URL = f'https://api.themoviedb.org/3/movie/top_rated?api_key={API_KEY}&language=ko-kr&page={API_PAGE}'
+
+# https://api.themoviedb.org/3/movie/top_rated?api_key=af5292844a6af1d68203e1c0b3104130&language=ko-kr&page=1
+
 
 
 @api_view(['GET'])
 def get_movies_API(request):
     if request.method == 'GET':
+        API_PAGE = 0 
+        while API_PAGE < 30:
+            
+            API_KEY = 'af5292844a6af1d68203e1c0b3104130'
+            API_PAGE += 1
+            API_URL = f'https://api.themoviedb.org/3/movie/top_rated?api_key={API_KEY}&language=ko-kr&page={API_PAGE}'
 
-        # API_URL에 요청하여 얻은 데이터
-        movies_API = requests.get(API_URL).json()
-        
-        # 내부 DB에서 호출한 데이터
-        movies_in_db = Movie.objects.all()
-        
-        # 한 페이지 내에 있는 데이터 분리
-        for movie_API in movies_API['results']:
+            print(API_PAGE)
 
-            try:
-                # 불러온 영화의 id를 기록
-                movie_id = movie_API['id']
-                # 불러온 영화의 id가 이미 DB 내에 존재하는 경우 continue
-                if movies_in_db.filter(movie_id=movie_id).exists():
-                    continue
 
-                # 데이터 형식과 일치 (id는 규칙에 의해 movie_id로 변경)
-                movie = Movie(
-                    adult=movie_API['adult'],
-                    backdrop_path=movie_API['backdrop_path'],
-                    genre_ids=movie_API['genre_ids'],
-                    movie_id=movie_API['id'],
-                    original_language=movie_API['original_language'],
-                    original_title=movie_API['original_title'],
-                    overview=movie_API['overview'],
-                    popularity=movie_API['popularity'],
-                    poster_path=movie_API['poster_path'],
-                    release_date=movie_API['release_date'],
-                    title=movie_API['title'],
-                    video=movie_API['video'],
-                    vote_average=movie_API['vote_average'],
-                    vote_count=movie_API['vote_count']
-                )
+            # API_URL에 요청하여 얻은 데이터
+            movies_API = requests.get(API_URL).json()
+            
+            # 내부 DB에서 호출한 데이터
+            movies_in_db = Movie.objects.all()
+            
+            # 한 페이지 내에 있는 데이터 분리
+            for movie_API in movies_API['results']:
 
-                movie.save()
+                try:
+                    
+                    # 불러온 영화의 id가 NULL인 경우 continue
+                    if movie_API['id'] is None:
+                        continue
+                    
+                    # 불러온 영화의 id를 기록
+                    movie_id = movie_API['id']
 
-            except Exception as e:
-                return Response({"result": "Error", "message": str(e)})
+                    # 불러온 영화의 id가 이미 DB 내에 존재하는 경우 continue
+                    if movies_in_db.filter(movie_id=movie_id).exists():
+                        continue
+
+
+                    # 데이터 형식과 일치 (id는 규칙에 의해 movie_id로 변경)
+                    movie = Movie(
+                        adult=movie_API['adult'],
+                        backdrop_path=movie_API['backdrop_path'],
+                        genre_ids=movie_API['genre_ids'],
+                        movie_id=movie_API['id'],
+                        original_language=movie_API['original_language'],
+                        original_title=movie_API['original_title'],
+                        overview=movie_API['overview'],
+                        popularity=movie_API['popularity'],
+                        poster_path=movie_API['poster_path'],
+                        release_date=movie_API['release_date'],
+                        title=movie_API['title'],
+                        video=movie_API['video'],
+                        vote_average=movie_API['vote_average'],
+                        vote_count=movie_API['vote_count']
+                    )
+
+                    movie.save()
+
+                except Exception as err:
+                    return Response({"result": "Error", "message": str(err)})
 
         return Response({"result": "OK"})
 
