2e0ba96a2c1749a98a64cf7ad6e9e07045280080
서준희 <seojoonhee10@gmail.com>
2023-05-19 12:02:05 +0900

    get token response user

--- final-pjt-back/articles/serializers.py
+++ final-pjt-back/articles/serializers.py
@@ -2,10 +2,13 @@ from rest_framework import serializers
 from .models import Article, Comment
 
 class ArticleListSerializer(serializers.ModelSerializer):
-    
+    username = serializers.CharField(source='user.username', read_only=True)
+
     class Meta:
         model = Article
-        fields = ('id', 'title', 'content')
+        # fields = ('id', 'title', 'content')
+        fields = ('id', 'title', 'content', 'user', 'username')
+
 
 class CommentSerializer(serializers.ModelSerializer):
     
@@ -13,12 +16,14 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = '__all__'
         read_only_fields = ('article',)
-    pass
+
 
 class ArticleSerializer(serializers.ModelSerializer):
     comment_set = CommentSerializer(many=True, read_only=True)
     comment_count = serializers.IntegerField(source='comment_set.count', read_only=True)
+    username = serializers.CharField(source='user.username', read_only=True)
 
     class Meta:
         model = Article
         fields = '__all__'
+        read_only_fields = ('user', )
--- final-pjt-back/articles/views.py
+++ final-pjt-back/articles/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import get_list_or_404, get_object_or_404
 from rest_framework import status
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-# permission Decorators
-from rest_framework.decorators import permission_classes
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Article, Comment
@@ -13,8 +10,9 @@ from .serializers import ArticleListSerializer, ArticleSerializer, CommentSerial
 
 
 @api_view(['GET', 'POST'])
-# @permission_classes([isAuthenticated])
+@permission_classes([IsAuthenticated])
 def article_list(request):
+
     if request.method == 'GET':
         articles = get_list_or_404(Article)
         serializer = ArticleListSerializer(articles, many=True)
@@ -23,12 +21,12 @@ def article_list(request):
     elif request.method == 'POST':
 
         # 이거를 DB > accounts_user 에서 일치하는 것을 찾아서 username을 추가해서 저장하면 된다.
-        print(request.data['token'])
+        # print(request.data['token'])
 
 
         serializer = ArticleSerializer(data=request.data)        
         if serializer.is_valid(raise_exception=True):
-            serializer.save()
+            serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
