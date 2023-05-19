6c021d370ffda9fed10e1ec517da15f424d7b2bd
서준희 <seojoonhee10@gmail.com>
2023-05-19 13:34:38 +0900

    movie view add

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('get/', views.get_movies_API),
+    path('get/movies/', views.get_movies_API),
+    path('get/now_playing_movies/', views.get_now_playing_movies_API),
+    path('get/upcoming_movies/', views.get_upcoming_movies_API),
     path('', views.movie_list),
 ]
