b0e5d981260e4fd7ed5a1015f37ce8ef2e682af4
seojoonhee <seojoonhee10@gmail.com>
2024-01-20 19:46:55 +0900

    240120 docker setting

--- final-pjt-back/back_api/settings.py
+++ final-pjt-back/back_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@c=1^c7yh%893f$zfdv^@eyi8(zo7jt)i%#maqt077twy9w4l3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.124.210.213', 'localhost', '127.0.0.1']
 
 
 # Application definition
